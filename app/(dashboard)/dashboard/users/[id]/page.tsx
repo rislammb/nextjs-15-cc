6c50dacce7219525ecfaff32441a355915dc2472
Rayhanul Islam <rislammb@gmail.com>
2024-11-13 16:21:07 +0600

    fixed params issue

--- app/(dashboard)/dashboard/users/[id]/page.tsx
+++ app/(dashboard)/dashboard/users/[id]/page.tsx
@@ -1,4 +1,4 @@
-export default function UserDetails({ params }: { params: { id: string } }) {
-  const { id } = params;
+export default async function UserDetails({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
   return <h1 className="text-3xl">User Profile {id}</h1>;
 }
