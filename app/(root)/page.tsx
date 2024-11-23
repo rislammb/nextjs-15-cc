b180d2c0ca1071e536bfd7f8663a1718a961be17
Rayhanul Islam <rislammb@gmail.com>
2024-11-23 19:23:25 +0600

    added search and filter functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,7 +9,8 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
-  const { data: posts } = await sanityFetch({ query: STARTUP_QUERY });
+  const params = { search: query || null };
+  const { data: posts } = await sanityFetch({ query: STARTUP_QUERY, params });
 
   return (
     <>
