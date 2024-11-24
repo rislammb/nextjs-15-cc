0deb15356bab0d3ed6fb227e5df8445ed615ff69
Rayhanul Islam <rislammb@gmail.com>
2024-11-24 08:59:14 +0600

    started startup details page

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -2,15 +2,31 @@ import { defineQuery } from "next-sanity";
 
 export const STARTUP_QUERY =
   defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author -> name match $search] | order(_createdAt desc) {
-  _id, 
-  title, 
-  slug,
-  image,
-  _createdAt,
-  author -> {
-    _id, name, image, bio
-  },
-  views,
-  description,
-  category
-}`);
+    _id, 
+    title, 
+    slug,
+    image,
+    _createdAt,
+    author -> {
+      _id, name, image, bio
+    },
+    views,
+    description,
+    category
+  }`);
+
+export const STARTUP_BY_ID_QUERY =
+  defineQuery(`*[_type == "startup" && _id == $id][0] {
+    _id, 
+    title, 
+    slug,
+    image,
+    _createdAt,
+    author -> {
+     _id, name, username, image, bio
+    },
+    views,
+    description,
+    category,
+    pitch
+  }`);
