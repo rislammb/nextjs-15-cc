2edaf75bd297d2c9321c3190eea347f03fe44eef
Rayhanul Islam <rislammb@gmail.com>
2024-11-25 17:17:31 +0600

    started create startup page

---
+++ components/StartupForm.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useState } from "react";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+
+export default function StartupForm() {
+  const [errors, setErrors] = useState<Record<string, string>>({});
+  return (
+    <form action={() => {}} className="startup-form">
+      <div>
+        <label htmlFor="title" className="startup-form_label">
+          Title
+        </label>
+        <Input
+          id="title"
+          name="title"
+          className="startup-form_input"
+          placeholder="Startup Title"
+          required
+        />
+        {errors.title && <p className="startup-form_error">{errors.title}</p>}
+      </div>
+
+      <div>
+        <label htmlFor="description" className="startup-form_label">
+          Description
+        </label>
+        <Textarea
+          id="description"
+          name="description"
+          className="startup-form_textarea"
+          placeholder="Startup Description"
+          required
+        />
+        {errors.description && (
+          <p className="startup-form_error">{errors.description}</p>
+        )}
+      </div>
+
+      <div>
+        <label htmlFor="category" className="startup-form_label">
+          Category
+        </label>
+        <Input
+          id="category"
+          name="category"
+          className="startup-form_input"
+          placeholder="Startup Category (Tech, Help, Education ...)"
+          required
+        />
+        {errors.category && (
+          <p className="startup-form_error">{errors.category}</p>
+        )}
+      </div>
+
+      <div>
+        <label htmlFor="link" className="startup-form_label">
+          Image URL
+        </label>
+        <Input
+          id="link"
+          name="link"
+          className="startup-form_input"
+          placeholder="Startup Image URL"
+          required
+        />
+        {errors.link && <p className="startup-form_error">{errors.link}</p>}
+      </div>
+    </form>
+  );
+}
